993341431b00aec6bea837811b4f68b90fe5ab3d
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-09 07:33:38 +0000

    Step1 and Step2 of Panel

--- Panel.py
+++ Panel.py
@@ -2,7 +2,17 @@ import streamlit as st
 import pandas as pd
 
 st.title("Panel")
-upload_file = st.file_uploader("Upload File", type=["csv", "json"])
-if upload_file is not None:
-  data = pd.read_csv(upload_file)
-  st.data_editor(data)
+
+tab1, tab2 = st.tabs(["Step 1", "Step 2"])
+
+with tab1:
+  st.header("Step 1. Upload File to Work")
+  with st.form(key="Upload_data"):
+    upload_file = st.file_uploader("Form of file", type=["csv"])
+    file_data = st.form_submit_button("Download data")
+
+with tab2:
+  st.header("Step 2. View the dataset")
+  if file_data == True:
+    data = pd.read_csv(upload_file)
+    st.data_editor(data)
