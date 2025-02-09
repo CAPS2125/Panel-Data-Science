76ed5b69d7f96df0b085e06a969171ed8a5aba34
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-09 07:02:06 +0000

    Test 1 Panel

--- Panel.py
+++ Panel.py
@@ -1,38 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-data = {
-  "Name": ["Christopher"],
-  "Age": [20],
-  "Student": [True]
-}
-
-# JSON Example with write
-st.write({"Key": "Value"})
-
-#Button Demo
-state = st.button("Press me")
-if state == True:
-  st.write(state)
-
-# Text Elements
-st.title("Title: Panel of Data Science")
-st.header("Header: Testing")
-st.subheader("Subheader: Testing the framework Streamlit")
-st.markdown("This is _Markdown_")
-st.caption("Small Text")
-code_example = """
-def add(a, b):
-  return a + b
-"""
-st.code(code_example, language="python")
-st.divider()
-
-# DataFrames
-st.subheader("Dataframe")
-df = {
-  "Name": ["Christopher", "Julian", "Lira"],
-  "Age": [20, 19, 20],
-  "Carrer": ["Data Science", "Computer Science", "Enginner ..."]
-}
-st.dataframe(df)
+st.title("Panel")
+upload_file = st.file_uploader("Upload File", type=["csv", "json"])
+if upload_file is not None:
+  data = pd.read_csv(upload_file)
+  st.data_editor(data)
