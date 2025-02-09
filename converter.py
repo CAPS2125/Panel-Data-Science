3fe7334e507c0ab277223e30e44258e172958e6e
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-08 17:46:08 -0800

    Add Text Elements to converter.py

--- converter.py
+++ converter.py
@@ -7,8 +7,22 @@ data = {
   "Student": [True]
 }
 
-st.title("Panel of Data Science")
+# Text Elements
+st.title("Title: Panel of Data Science")
+st.header("Header: Testing")
+st.subheader("Subheader: Testing the framework Streamlit")
+st.markdown("This is _Markdown_")
+st.caption("Small Text")
+code_example = """
+def add(a, b):
+  return a + b
+"""
+st.code(code_example, languaje="python")
+st.divider()
+
+# JSON Example with write
 st.write({"Key": "Value"})
+#Button Demo
 state = st.button("Press me")
 if state == True:
   st.write(state)
