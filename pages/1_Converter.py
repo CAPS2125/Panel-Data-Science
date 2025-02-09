74e7c47b3ff23cce7f17ff53a0a71f936af9b240
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-08 19:04:15 -0800

    Testing upload a file 1_Converter.py

--- pages/1_Converter.py
+++ pages/1_Converter.py
@@ -4,4 +4,5 @@ st.title("Converter CSV <-> JSON")
 
 with st.form(key="Input_Data"):
   format = st.selectbox("Select the format", ["CSV", "JSON"])
+  uploaded_file = st.file_uploader("Upload a file", type=["csv", "json"])
   st.form_submit_button()
