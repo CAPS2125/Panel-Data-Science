53ba20191d4fcac1af752f2a33201a5f4131a548
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-08 19:00:55 -0800

    Testing selectbox 1_Converter.py

--- pages/1_Converter.py
+++ pages/1_Converter.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Converter CSV <-> JSON")
+
+with st.form(key="Input_Data"):
+  format = st.selectbox("Select the format", ["CSV", "JSON"])
+  st.form_submit_button()
