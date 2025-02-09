c626a2b54b1edad26fb01f14f728e6cef44a4039
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-08 19:24:10 -0800

    is or not 1_Converter.py

--- pages/1_Converter.py
+++ pages/1_Converter.py
@@ -4,7 +4,7 @@ st.title("Converter CSV <-> JSON")
 
 with st.form(key="Input_Data"):
   format_to = st.radio("Select the format Origin", ["CSV to JSON", "JSON to CSV"])
-  if format_to == "CSV to JSON":
+  if format_to is "CSV to JSON":
     orient_json = st.selectbox("Select the order of JSON", ["table", "records", "index", "split", "columns", "values"])
   else:
     None
