397f54de0558c34e8df7de52d98823f70283c4cb
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-08 19:34:46 -0800

    Update 1_Converter.py

--- pages/1_Converter.py
+++ pages/1_Converter.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 st.title("Converter CSV <-> JSON")
 
+option_orient = st.checkbox("Change orient of JSON")
+
 with st.form(key="Input_Data"):
   format_to = st.radio("Select the format Origin", ["CSV to JSON", "JSON to CSV"])
-  if st.checkbox("Change orient of JSON", value=False) == True:
+  if option_orient:
       orient_json = st.selectbox("Select the order of JSON", ["table", "records", "index", "split", "columns", "values"])
   uploaded_file = st.file_uploader("Upload a file", type=["csv", "json"])
   st.form_submit_button()
