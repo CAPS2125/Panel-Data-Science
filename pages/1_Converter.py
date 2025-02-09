54e4a96bea9f1ded6cbe090757f2324f21301672
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-08 19:21:22 -0800

    None without select first option 1_Converter.py

--- pages/1_Converter.py
+++ pages/1_Converter.py
@@ -6,5 +6,7 @@ with st.form(key="Input_Data"):
   format_to = st.radio("Select the format Origin", ["CSV to JSON", "JSON to CSV"])
   if format_to == "CSV to JSON":
     orient_json = st.selectbox("Select the order of JSON", ["table", "records", "index", "split", "columns", "values"])
+  else:
+    None
   uploaded_file = st.file_uploader("Upload a file", type=["csv", "json"])
   st.form_submit_button()
