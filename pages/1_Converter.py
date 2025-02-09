473df75aa07f41b58492b36699bd6895dd52ec7d
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-08 19:31:32 -0800

    Test Checkbox 1_Converter.py

--- pages/1_Converter.py
+++ pages/1_Converter.py
@@ -4,6 +4,7 @@ st.title("Converter CSV <-> JSON")
 
 with st.form(key="Input_Data"):
   format_to = st.radio("Select the format Origin", ["CSV to JSON", "JSON to CSV"])
-  orient_json = st.selectbox("Select the order of JSON", ["table", "records", "index", "split", "columns", "values"])
+  if st.checkbox("Change orient of JSON") is not None:
+    orient_json = st.selectbox("Select the order of JSON", ["table", "records", "index", "split", "columns", "values"])
   uploaded_file = st.file_uploader("Upload a file", type=["csv", "json"])
   st.form_submit_button()
