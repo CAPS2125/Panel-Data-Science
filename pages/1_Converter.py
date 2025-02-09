04f21ce57a1f33e4492e13c84c63e295abc91e3a
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-08 19:29:46 -0800

    Not Dynamic 1_Converter.py

--- pages/1_Converter.py
+++ pages/1_Converter.py
@@ -4,8 +4,6 @@ st.title("Converter CSV <-> JSON")
 
 with st.form(key="Input_Data"):
   format_to = st.radio("Select the format Origin", ["CSV to JSON", "JSON to CSV"])
-  button_orient = st.button("Change orient of json")
-  if button_orient == True:
-    orient_json = st.selectbox("Select the order of JSON", ["table", "records", "index", "split", "columns", "values"])
+  orient_json = st.selectbox("Select the order of JSON", ["table", "records", "index", "split", "columns", "values"])
   uploaded_file = st.file_uploader("Upload a file", type=["csv", "json"])
   st.form_submit_button()
