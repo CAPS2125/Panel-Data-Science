92b5b76e2123ecfcff20a27e8e7b55ffe17d9762
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-02-10 02:31:54 +0000

    Dataset information V. 1

--- Panel.py
+++ Panel.py
@@ -4,7 +4,8 @@ import pandas as pd
 st.set_page_config(page_title="Main Panel")
 st.title("Panel")
 
-tab1, tab2 = st.tabs(["Step 1", "Step 2"])
+data = None
+tab1, tab2, tab3 = st.tabs(["Step 1", "Step 2", "Step 3"])
 
 with tab1:
   st.header("Step 1. Upload File to Work")
@@ -17,3 +18,26 @@ with tab2:
   if file_data == True:
     data = pd.read_csv(upload_file)
     st.data_editor(data)
+
+with tab3:
+  st.header("Step 3. Dataset information")
+  if data is not None:
+    col1, col2, col3 = st.columns(3)
+    with col1:
+      dtypes_df = data.dtypes.to_frame(name='dtypes')
+      st.metric(label="Rows", value=data.shape[0])
+      st.write("Data types")
+      st.dataframe(dtypes_df)
+    with col2:
+      data_unique_counts = data.nunique().to_frame(name='count')
+      st.metric(label="Columns", value=data.shape[1])
+      st.write("Number of unique values")
+      st.dataframe(data_unique_counts)
+
+    with col3:
+      amount_data = data.shape[0] * data.shape[1]
+      null_check = pd.DataFrame(data.isnull().any(), columns=['Nulls'])
+      st.metric(label="Amount of data", value=amount_data)
+      st.write("Does it have null values?")
+      st.dataframe(null_check)
+      
